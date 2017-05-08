dd2f6bb2d2e95602a70991910627e39bce18ca70
jali-clarke <jinnah.ali-clarke@outlook.com>
2017-05-08 00:57:22 -0400

    finished HistoryT for now?

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # history-monad
 Like a state monad, but it stores a list of saved states.
+
+Use "bookmark" to create a snapshot of the current state and push the snapshot onto
+the stack.  Use "rewind" to try to revert to the previous state.
+
+The usual "get" and "set", as well as other MonadState-supported actions, will act
+using only the current state.
+
+Later on I'll upgrade this to use a tree of states, analogous to multiple timelines.
--- src/HistoryT.hs
+++ src/HistoryT.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, MultiParamTypeClasses #-}
 
 module HistoryT (
     HistoryT(),
@@ -11,6 +11,7 @@ module HistoryT (
 import Control.Monad.Identity
 import Control.Monad.Trans
 import Control.Monad.State.Lazy
+import Control.Monad.State.Class
 
 newtype HistoryT s m a = HistoryT {runHistoryT' :: StateT [s] m a} deriving (Functor, Applicative, Monad, MonadTrans)
 
@@ -21,3 +22,16 @@ type History s = HistoryT s Identity
 
 runHistory :: History s a -> (s -> (a, [s]))
 runHistory comp = runIdentity . runHistoryT comp
+
+instance Monad m => MonadState s (HistoryT s m) where
+    get = HistoryT (gets head) -- heh
+    put s = HistoryT (modify $ (s :) . tail)
+
+bookmark :: Monad m => HistoryT s m s
+bookmark = HistoryT . state $ \ss@(s : _) -> (s, s : ss)
+
+rewind :: Monad m => HistoryT s m (Maybe s)
+rewind = HistoryT . state $ \ss ->
+    case ss of
+        (_ : []) -> (Nothing, ss)
+        (_ : ss'@(s' : _)) -> (Just s', ss')
