61436a9f6c7b33d548fd3950263bd434d8e984e3
jali-clarke <jinnah.ali-clarke@outlook.com>
2017-05-24 16:23:13 -0400

    changed to free monad approach :^)

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,16 @@ module Main where
 
 import History
 
+-- numOps should be 2, flip runHistory 0 should return 3
+test :: History Int Int
+test = do
+    i <- get
+    put (i + 1)
+    i' <- get
+    put (i + 2)
+    get
+
 main :: IO ()
-main = return ()
+main = do
+    print (numOps test)
+    print (runHistory test 0)
--- src/History.hs
+++ src/History.hs
@@ -3,32 +3,16 @@
 module History (
     History,
     runHistory,
+    numOps,
 
     module Control.Monad.State.Lazy
 ) where
 
 import Control.Monad.State.Lazy
 
-data History s a = History (s -> a) ([s -> s] -> [s -> s])
+data History s a = Pure a | Modify (s -> s) (History s a) | Get (s -> History s a)
 
 runHistory :: History s a -> s -> a
-runHistory (History proj stack) = proj . foldr (.) id (stack [])
-
-instance Functor (History s) where
-    fmap f (History proj stack) = History (f . proj) stack
-
-instance Applicative (History s) where
-    pure a = History (const a) id
-    History projf stackf <*> History proja stacka = History (projf <*> proja) (stacka . stackf)
-
-instance Monad (History s) where
-    History proja stacka >>= g = History projb stackb
-        where
-            projh = g . proja
-            projb s = let History projb' _ = projh s in projb' s
-            stackb = undefined
-
-instance MonadState s (History s) where
-    get = History id id
-    put s = History (const ()) (const s :)
-    state func = History (fst . func) (snd . func :)
+runHistory (Pure a) _ = a
+runHistory (Modify f newHist) s = runHistory newHist (f s)
+runHistory (Get f) s = runHistory (f s)
